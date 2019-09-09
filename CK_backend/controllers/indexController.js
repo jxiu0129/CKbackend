6564a7d2392203f88436a727a0856a605a35318d
HappyFeet07 <jack0978239920@gmail.com>
2019-09-09 11:41:38 +0800

    small

--- CK_backend/controllers/indexController.js
+++ CK_backend/controllers/indexController.js
@@ -18,14 +18,15 @@ exports.index = function(req,res){
 exports.login_index = function(req, res){
     console.log('location.code : ' + req.query.code);
     API_LoginCode = req.query.code;
-    // req.session.API_LoginCode = req.query.code;
+    req.session.API_LoginCode = req.query.code;
 
     rp.get('http://wm.nccu.edu.tw:3001/oauth/access_token?grant_type=access_token&client_id=bcdhjsbcjsdbc&redirect_uri=http://localhost:3000/login_index&code=' + API_LoginCode, function(req,res, body){
         API_Access = JSON.parse(body);
-        // req.session.API_Access = API_Access;
+        req.session.API_Access = API_Access;
     })
     .catch(() => {
         console.log('wrong');
+        console.log(API_Access);
         console.log(API_Access.access_token);
         console.log(API_Access.refresh_token);
         rp.get('http://wm.nccu.edu.tw:3001/openapi/user_info', {
@@ -35,7 +36,7 @@ exports.login_index = function(req, res){
         })
         .then((message) => {
             console.log(message);
-            // req.session.user_info = message;
+            req.session.user_info = message;
         })
         .catch(() =>{
             console.log('fail');
@@ -45,29 +46,6 @@ exports.login_index = function(req, res){
     res.render('login_index');
 };
 
-/*const getAPI_Access = (logincode) => new Promise((resolve, reject) => {
-    let API_stuff;
-    rp.get('http://wm.nccu.edu.tw:3001/oauth/access_token?grant_type=access_token&client_id=bcdhjsbcjsdbc&redirect_uri=http://localhost:3000/login_index&code=' + logincode,(err, res, body) => {
-        console.log(JSON.parse(body));
-        API_stuff = JSON.parse(body);
-    });
-    if(API_stuff !== undefined){
-        resolve(API_stuff);
-    }else{
-        reject("Nope");
-    }
-});*/
-
-/*let getAPI_UserInfo = (access_token) => {
-    let UserData;
-    rp.get('http://wm.nccu.edu.tw:3001/openapi/user_info')
-    .auth(null, null, true, access_token.access_token)
-    .then((res,body) =>{
-        console.log(JSON.parse(body));
-        UserData = JSON.parse(body);
-    });
-    return UserData;
-};*/
 
 
 

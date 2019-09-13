6a00d65e6ec2a68e42f7980324c2e0011abc9746
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2019-09-13 23:28:57 +0800

    change user/myrecord  (session not yet)

--- CK_backend/controllers/userController.js
+++ CK_backend/controllers/userController.js
@@ -1,4 +1,4 @@
-//const User = require("../models/user");
+const User = require("../models/user");
 const Event = require("../models/event");
 const Attendance = require('../models/attendance');
 const async = require('async');
@@ -10,37 +10,30 @@ exports.user = function(req,res){
 
 // connect to myrecords.ejs
 // user routes 裡有個test 去上傳測試用資料
-exports.user_record = (req,res, next) => {
-    
-    let event_list = [];
 
-    Attendance.find({}, '_id name sign_in sign_out income', (err, data) => {
+exports.user_record = (req, res, next) => {
+    
+    User
+    .findById(req.query._id, 'attend')
+    // using http://localhost:3000/user/record?_id=5d70d857107eb226b4b8f770 for test
+    // .populate('hold.holded_events')
+    .populate('attend.event_id')
+    .exec((err, data) => {
         if(err){ console.log(err); }
-        console.log('here!!!=>'+data);
-        if(data[0].name == null){
+        console.log('here!!!=>'+ data.attend);
+
+        if(data._id == null){
             let err = new Error('你還沒有參加任何活動喔');
             err.status = 404;
             return next(err);
         }
 
-        for(let count of data){
-            event_list.push(count._id);
-        }
 
-        console.log(event_list);
-        res.render('user/myrecords', {event_info: data});
+        res.render('user/myrecords', {event_info: data.attend});
         // res.send('fuck');
+        // res.send(data);
     });
-
-    // if(event_list[0]){
-    //     for(let id of event_list){
-    //         Event.findById(id, 'name time sponsor', (err, data) => {
-    //             if(err){console.log(err);}
-    //             console.log('data in loop => '+data);
-    //             res.render('myrecords', {event_specific_info: data, id:id});
-    //         });
-    //     }
-    // }
+    
 };
 
 exports.user_events = function(req,res){

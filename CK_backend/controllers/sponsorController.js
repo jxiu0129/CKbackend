332295be05ae6266012f913dd58a5c1faefec67b
Albert Pao <albert90162@gmail.com>
2019-10-11 23:04:33 +0800

    1.npm install moment 2.create event can set status to holding

--- CK_backend/controllers/sponsorController.js
+++ CK_backend/controllers/sponsorController.js
@@ -7,6 +7,7 @@ const async = require("async");
 const request = require('request');
 const QRCode = require('qrcode');
 const schedule = require('node-schedule');
+const moment = require('moment');
 
 
 const { body,validationResult } = require('express-validator/check');
@@ -30,7 +31,111 @@ let updateStaus = function(){
 
 updateStaus();
 
+// 2.克制化時間格式 (年 月 日 上/下午 )
 
+moment.locale('zh-tw', {
+    months: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
+    monthsShort: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
+    weekdays: '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
+    weekdaysShort: '周日_周一_周二_周三_周四_周五_周六'.split('_'),
+    weekdaysMin: '日_一_二_三_四_五_六'.split('_'),
+    longDateFormat: {
+      LT: 'Ah點mm分',
+      LTS: 'Ah點m分s秒',
+      L: 'YYYY-MM-DD',
+      LL: 'YYYY年MMMD日',
+      LLL: 'YYYY年MMMD日Ah點mm分',
+      LLLL: 'YYYY年MMMD日ddddAh點mm分',
+      l: 'YYYY-MM-DD',
+      ll: 'YYYY年MMMD日',
+      lll: 'YYYY年MMMD日Ah點mm分',
+      llll: 'YYYY年MMMD日ddddAh點mm分'
+    },
+    meridiemParse: /凌晨|早上|上午|中午|下午|晚上/,
+    meridiemHour: function (h, meridiem) {
+      let hour = h;
+      if (hour === 12) {
+        hour = 0;
+      }
+      if (meridiem === '凌晨' || meridiem === '早上' ||
+        meridiem === '上午') {
+        return hour;
+      } else if (meridiem === '下午' || meridiem === '晚上') {
+        return hour + 12;
+      } else {
+        // '中午'
+        return hour >= 11 ? hour : hour + 12;
+      }
+    },
+    meridiem: function (hour, minute, isLower) {
+      const hm = hour * 100 + minute;
+      if (hm < 1200) {
+        return '上午';
+      } else {
+        return '下午';
+      }
+    },
+    calendar: {
+      sameDay: function () {
+        return this.minutes() === 0 ? '[今天]Ah[點整]' : '[今天]LT';
+      },
+      nextDay: function () {
+        return this.minutes() === 0 ? '[明天]Ah[點整]' : '[明天]LT';
+      },
+      lastDay: function () {
+        return this.minutes() === 0 ? '[昨天]Ah[點整]' : '[昨天]LT';
+      },
+      nextWeek: function () {
+        let startOfWeek, prefix;
+        startOfWeek = moment().startOf('week');
+        prefix = this.diff(startOfWeek, 'days') >= 7 ? '[下]' : '[本]';
+        return this.minutes() === 0 ? prefix + 'dddA點整' : prefix + 'dddAh點mm';
+      },
+      lastWeek: function () {
+        let startOfWeek, prefix;
+        startOfWeek = moment().startOf('week');
+        prefix = this.unix() < startOfWeek.unix() ? '[上]' : '[本]';
+        return this.minutes() === 0 ? prefix + 'dddAh點整' : prefix + 'dddAh點mm';
+      },
+      sameElse: 'LL'
+    },
+    ordinalParse: /\d{1,2}(日|月|周)/,
+    ordinal: function (number, period) {
+      switch (period) {
+        case 'd':
+        case 'D':
+        case 'DDD':
+          return number + '日';
+        case 'M':
+          return number + '月';
+        case 'w':
+        case 'W':
+          return number + '周';
+        default:
+          return number;
+      }
+    },
+    relativeTime: {
+      future: '%s内',
+      past: '%s前',
+      s: '幾秒',
+      m: '1 分鐘',
+      mm: '%d 分鐘',
+      h: '1 小時',
+      hh: '%d 小時',
+      d: '1 天',
+      dd: '%d 天',
+      M: '1 個月',
+      MM: '%d 个月',
+      y: '1 年',
+      yy: '%d 年'
+    },
+    week: {
+      // GB/T 7408-1994《数据元和交换格式·信息交换·日期和时间表示法》与ISO 8601:1988等效
+      dow: 1, // Monday is the first day of the week.
+      doy: 4  // The week that contains Jan 4th is the first week of the year.
+    }
+  });
 
 exports.sponsor_events= async(req,res,next) =>{
 
@@ -41,15 +146,25 @@ exports.sponsor_events= async(req,res,next) =>{
         if (err) { return next(err); }
         
         Event.find({_id:_user.hold.holded_events},'_id shortid name time endtime location expense amount status')
-        .sort([['time','descending']])
+        .sort([['status','descending']])
         .exec(async (err,list_event) => {
-            // console.log(list_event);
             if (err) { return next(err); }
             if (list_event.length == 0){
                 res.render('sponsor/myevents_noevent', { username: req.session.user_info.user_info.name});
-
             }else{
-                res.render('sponsor/myevents', { username: req.session.user_info.user_info.name,title: 'My Events | NCCU Attendance', list_event:  list_event});
+                let timeArray = [];
+                let endtimeArray = [];
+                for(let i =0 ; i< list_event.length;i++){
+                    timeArray.push(moment(list_event[i].time).format('LLL'));
+                    endtimeArray.push(moment(list_event[i].endtime).format('LLL'));
+                }
+                res.render('sponsor/myevents', { 
+                    username: req.session.user_info.user_info.name,
+                    title: 'My Events | NCCU Attendance',
+                    list_event:  list_event,
+                    Time : timeArray,
+                    endTime : endtimeArray
+                });
             }
 
                     
@@ -117,7 +232,6 @@ exports.sponsor_create_post = [
         // };
         // Create a genre object with escaped and trimmed data.
         let _user = await User.findOne({email:req.session.user_info.user_info.email});
-
         let event = new Event({
             // _id : req.body._id, 
             holder : _user._id,
@@ -129,7 +243,9 @@ exports.sponsor_create_post = [
             amount : 0,
             ncculink : req.body.event_link,
         });
-
+        if (event.time -  Date.now() <= 3600000){
+            event.status = 'holding';
+        }
 
         if (!errors.isEmpty()) {
             // There are errors. Render the form again with sanitized values/error messages.
@@ -360,6 +476,7 @@ exports.SignIn_create_post= [
     //     }
     //     return true;
     // }),
+
 
 
     // // Sanitize fields.

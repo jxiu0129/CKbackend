ebf30d149370fab8f16cdf62b2b80e61a7fbc2e3
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2019-11-26 01:52:07 +0800

    修改 [user record] [user record info] 改成新前端：phone size

--- CK_backend/controllers/userController.js
+++ CK_backend/controllers/userController.js
@@ -11,17 +11,20 @@ exports.user = function(req,res){
 // connect to myrecords.ejs
 // user routes 裡有個test 去上傳測試用資料
 
-exports.user_record = (req, res, next) => {
-    req.session.reload();
-    console.log('session HERE --->' + req.session.user_info.user_info.email);
+exports.user_record = async (req, res, next) => {
+    await req.session.reload(err => {
+        if (err){ 
+            console.log('userRecord sess err', err);
+        }
+    });
     User
     .findOne({email:req.session.user_info.user_info.email}, 'attend email')
     .populate('attend.event_id')
     // .sort([['attend.event_id.time', 'descending']]) 
     .exec((err, data) => {
         if(err){ console.log(err); }
-        console.log('data ==>'+data);
-        console.log('data.attend ==>'+ data.attend);
+        // console.log('data ==>' + data);
+        // console.log('data.attend ==>'+ data.attend);
 
         if(data.attend == null){
             let err = new Error('你還沒有參加任何活動喔');
@@ -30,9 +33,11 @@ exports.user_record = (req, res, next) => {
         }
 
 
-        res.render('user/myrecords', {event_info: data.attend, username: req.session.user_info.user_info.name, url:req.session.API_LoginCode});
-        // res.send('fuck');
-        // res.send(req.session);
+        res.render('user/myrecords', {
+            event_info: data.attend, 
+            username: req.session.user_info.user_info.name, 
+            url:req.session.API_LoginCode
+        });
     });
     
 };
@@ -40,21 +45,20 @@ exports.user_record = (req, res, next) => {
 exports.user_events = function(req,res){
     res.render('root/index' , { title : "我的活動"});
 };
-
-// connect to myrecords.ejs
-
-
-
-//Albert 
+// Albert
 //eventinfo_record
-exports.eventinfo_record = (req,res)=>{
-    req.session.reload();
-    Event.findById(req.params.eventid,(err,event)=>{
+exports.eventinfo_record = async (req,res)=>{
+    await req.session.reload(err => {
+        if (err){ 
+            console.log('eventInfoRecord sess err', err);
+        }
+    });
+    Event.findById(req.query.eventid,(err,event)=>{
+        // console.log('EVENTINFO: ', event);
         res.render('user/eventinfo_record',{
             url:req.session.API_LoginCode,
             username:req.session.user_info.user_info.name,
             event : event,
-            
         });
     });
 };
